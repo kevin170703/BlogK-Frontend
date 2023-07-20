e2afbef8a988d1e1de93f3ca7ab17b83388e4a2f
kevin170703 <correakevinfabian01@gmail.com>
2023-07-20 14:06:49 -0300

    añadiendo google anality

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,6 +11,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-FQJG4LWVG7"
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-FQJG4LWVG7');
+        </script>
+      </head>
       <body>{children}</body>
     </html>
   );
